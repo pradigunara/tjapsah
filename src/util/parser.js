20425858007b56ffb410b0ef72e29aa7b180d0bf
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2020-05-19 10:49:00 +0700

    add card constant & parser (#1)
    Co-authored-by: Prawira Adiguna Pangestu <papangestu@indodana.com>

---
+++ src/util/parser.js
@@ -0,0 +1,36 @@
+import {
+  rankByCode,
+  suiteByCode,
+  codeByRank,
+  codeBySuite,
+} from 'src/constant/card'
+
+/**
+ * Parse 2-character card code
+ *
+ * @param {String} cardCode
+ */
+export const deserialize = (cardCode) => {
+  const [suiteCode, rankCode] = cardCode
+
+  return {
+    suite: suiteByCode[suiteCode],
+    rank: rankByCode[rankCode],
+  }
+}
+
+/**
+ * Stringify card object to card code
+ *
+ * @param {Object} cardObject
+ * @param {String} cardObject.suite
+ * @param {String} cardObject.rank
+ */
+export const serialize = (cardObject) => {
+  return codeBySuite[cardObject.suite] + codeByRank[cardObject.rank]
+}
+
+export default {
+  serialize,
+  deserialize,
+}
