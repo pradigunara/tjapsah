f17a16556514aea90c99938e6c5d4c9d29f6ab39
andreasanggaraa <andreasanggaraa@gmail.com>
2020-05-24 18:46:05 +0700

    reformat jsx file

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -2,45 +2,45 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 
 export default function Board() {
-    return (
-        <div className="board">
-            <Grid textAlign="center" className="board-grid" columns='equal'>
-                <Grid.Row>
-                    <Grid.Column>
-                        <div className="test2">Nothing Here</div>
-                    </Grid.Column>
-                    <Grid.Column stretched width={8}>
-                        <div className="test2">Opponent's Hand</div>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div className="test2">Nothing Here</div>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row stretched>
-                    <Grid.Column>
-                        <div className="test">Opponent's Hand</div>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div className="test">Field</div>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div className="test">Opponent's Hand</div>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column stretched>
-                        <div className="test3">Chatbox</div>
-                    </Grid.Column>
-                    <Grid.Column className="test3" stretched>
-                        <Grid.Row>1row</Grid.Row>
-                        <Grid.Row>2row</Grid.Row>
-                        <Grid.Row>2row</Grid.Row>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div className="test3">Nothing Here</div>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </div>
-    )
+  return (
+    <div className="board">
+      <Grid textAlign="center" className="board-grid" columns="equal">
+        <Grid.Row>
+          <Grid.Column>
+            <div className="test2">Nothing Here</div>
+          </Grid.Column>
+          <Grid.Column stretched width={8}>
+            <div className="test2">Opponent's Hand</div>
+          </Grid.Column>
+          <Grid.Column>
+            <div className="test2">Nothing Here</div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <div className="test">Opponent's Hand</div>
+          </Grid.Column>
+          <Grid.Column>
+            <div className="test">Field</div>
+          </Grid.Column>
+          <Grid.Column>
+            <div className="test">Opponent's Hand</div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column stretched>
+            <div className="test3">Chatbox</div>
+          </Grid.Column>
+          <Grid.Column className="test3" stretched>
+            <Grid.Row>1row</Grid.Row>
+            <Grid.Row>2row</Grid.Row>
+            <Grid.Row>2row</Grid.Row>
+          </Grid.Column>
+          <Grid.Column>
+            <div className="test3">Nothing Here</div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  )
 }
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -2,5 +2,10 @@ import React from 'react'
 import { Image } from 'semantic-ui-react'
 
 export default function Card({ cardName }) {
-    return <Image className="card-image" src={require(`assets/cards/${cardName}.svg`)} />
+  return (
+    <Image
+      className="card-image"
+      src={require(`assets/cards/${cardName}.svg`)}
+    />
+  )
 }
